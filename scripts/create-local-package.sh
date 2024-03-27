34260e1d4f63413332d220b1aec5ff566a81b14e
smac89 <8305511+smac89@users.noreply.github.com>
2024-03-27 12:08:30 -0600

    update scripts and readme

--- scripts/create-local-package.sh
+++ scripts/create-local-package.sh
@@ -4,10 +4,12 @@
 set -e
 
 if [ -n "$1" ]; then
+  echo "Creating archive for $1"
   git archive --format=zip --prefix=AutoDarkLinux/ "$1" --output AutoDarkLinux.sublime-package
 else
   echo "No version specified. Archiving main branch" >&2
   git archive --format=zip main --output AutoDarkLinux.sublime-package
 fi
 
+unzip -l AutoDarkLinux.sublime-package
 echo -e '\n===Archive created. Copy it to your "Installed Packages" folder===\n'
--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-[ -z "$1" ] && echo "No version specified" && exit 1
+[ -z "$1" ] && echo "No version specified" >&2 && exit 1
 git tag "$1"
 echo -e '\n===Tag created. Run `git push --tags`===\n'
